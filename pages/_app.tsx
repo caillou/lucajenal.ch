43912c0b586c10d96ea45aa038a4c5c8e12b1f94
Pierre Spring <pierre.spring@caillou.ch>
2021-01-16 12:16:36 +0100

    Use tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ElementType } from 'react'
+import '../styles/globals.css'
+
+type AppParameter<P> = {
+  Component: ElementType,
+  pageProps: P
+}
+
+function MyApp<P>({ Component, pageProps }: AppParameter<P>) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
