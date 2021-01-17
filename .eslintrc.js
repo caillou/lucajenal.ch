3afb1b1de5383a8c5df3707bdb48579600b1b713
Pierre Spring <pierre.spring@caillou.ch>
2021-01-17 10:10:00 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
+        'prettier/@typescript-eslint', // Prettier plugin
+        'plugin:prettier/recommended', // Prettier recommended rules
       ],
       rules: {
         // We will use TypeScript's types for component props instead
